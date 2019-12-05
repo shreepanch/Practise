4d2bcd20ad92b1cd7807513e800645188f14e535
praveenpcpc0000 <praveenpcpc0000@gmail.com>
2019-12-05 22:27:34 +0545

    first commit

---
+++ hello.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>My first php page</h1>
+<?php
+ECHO "Hwllo!<br>";   
+echo "hello<br>";
+EcHo "Hello!<br>";
+?>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>This is PHP Variable. </h1>
+<?php 
+$txt = "Hello";
+$x = 5;
+$y = 10;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+
+
+?>
+</body>
+</html>
