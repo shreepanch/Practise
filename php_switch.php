e5f8e6d39cfed3659cac5129227ba2eda6d4c62b
praveenpcpc0000 <praveenpcpc0000@gmail.com>
2019-12-16 05:25:17 +0545

    gg

---
+++ php_switch.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<html>
+<?php 
+$favcolor = "red";
+switch($favcolor){
+    case "red":
+    echo "Your fav color is red";
+    break;
+    case "blue":
+        echo "Your fav color is blue";
+    break;
+    case "Yellow":
+        echo "Your fav color is Yellow";
+    break;
+    case "green":
+        echo "Your fav color is green";
+    break;
+    default:
+    echo "Your fav color is neither red, blue, Yellow, or green";
+}
+?>
+</body>
+</html>
