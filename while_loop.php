0afa9850bb94c0766222f9ff74be592a9d6c117c
praveenpcpc0000 <praveenpcpc0000@gmail.com>
2019-12-19 15:09:24 +0545

    gg

---
+++ while_loop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+// $x = 1;
+// while($x <= 5){
+//     echo "The number is: $x <br>";
+//     $x++;
+// }
+$x = 0;
+while($x <= 100){
+    echo "The numbers is: $x <br>";
+    $x += 10;
+}
+
+
+?>
+</body>
+</html>
