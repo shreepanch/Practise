8d6da5ae2c7a15e001f5ff5987e4d220704e953a
praveenpcpc0000 <praveenpcpc0000@gmail.com>
2020-03-18 15:36:09 +0545

    php_form

---
+++ form.php
@@ -0,0 +1,12 @@
+<!DOCTYPR html>
+<html>
+<body>
+<form action="form.php" method="post">
+Name: <input type="text" name="name"><br>
+Email: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
+</body>
+</html>
