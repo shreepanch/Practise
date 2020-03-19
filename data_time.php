f2dfa48350a9f10525a6d663ff34d7afdc757ea2
praveenpcpc0000 <praveenpcpc0000@gmail.com>
2020-03-19 14:43:58 +0545

    data and time

---
+++ data_time.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "Todays is " . date("Y/m/d") . "<br>";
+echo "Todays is " . date("Y-m-d") . "<br>";
+echo "Todays is " . date("Y.m.d") . "<br>";
+?>
+</body>
+</html>
